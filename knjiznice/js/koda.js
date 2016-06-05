6b593d76d8eb50e887510f4de355ce8af2c7966a
Ajda Delic <ajda.delic@gmail.com>
2016-06-05 21:32:29 +0000

    Delo na indexu

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -32,11 +32,208 @@ function getSessionId() {
  */
 function generirajPodatke(stPacienta) {
   ehrId = "";
+  var ime,priimek,datumRojstva,krajBivanja;
+  var sessionId=getSessionId();
+  
+  if(stPacienta == 1){
+  	ime="Anton";
+  	priimek="Sova";
+  	datumRojstva="12.11.1965";
+  	krajBivanja="Goriška";
+  	
+  }else if(stPacienta == 2){
+  	ime="Gal";
+  	priimek="Novak";
+  	datumRojstva="9.6.1995";
+  	krajBivanja="Osrednjeslovenska";
+  }else if(stPacienta == 3){
+  	ime="Valerija";
+  	priimek="Koritnik";
+  	datumRojstva="25.1.1989";
+  	krajBivanja="Pomurska";
+  }
 
-  // TODO: Potrebno implementirati
+  	$.ajaxSetup({
+		    headers: {"Ehr-Session": sessionId}
+		});
+		$.ajax({
+		    url: baseUrl + "/ehr",
+		    type: 'POST',
+		    success: function (data) {
+		        var ehrId = data.ehrId;
+		        var partyData = {
+		            firstNames: ime,
+		            lastNames: priimek,
+		            dateOfBirth: datumRojstva,
+		            address: krajBivanja;
+		            partyAdditionalInfo: [{key: "ehrId", value: ehrId}]
+		        };
+		        $.ajax({
+		            url: baseUrl + "/demographics/party",
+		            type: 'POST',
+		            contentType: 'application/json',
+		            data: JSON.stringify(partyData),
+		            success: function (party) {
+		                if (party.action == 'CREATE') {
+		                    $("#kreirajSporocilo").html("<span class='obvestilo " +
+                          "label label-success fade-in'>Uspešno kreiran EHR '" +
+                          ehrId + "'.</span>");
+		                    $("#preberiEHRid").val(ehrId);
+		                }
+		            },
+		            error: function(err) {
+		            	$("#kreirajSporocilo").html("<span class='obvestilo label " +
+                    "label-danger fade-in'>Napaka '" +
+                    JSON.parse(err.responseText).userMessage + "'!");
+		            }
+		        });
 
   return ehrId;
 }
 
 
 // TODO: Tukaj implementirate funkcionalnost, ki jo podpira vaša aplikacija
+
+
+
+
+//
+
+
+/**
+ * Kreiraj nov EHR zapis za pacienta in dodaj osnovne demografske podatke.
+ * V primeru uspešne akcije izpiši sporočilo s pridobljenim EHR ID, sicer
+ * izpiši napako.
+ */
+function kreirajEHRzaBolnika() {
+	sessionId = getSessionId();
+
+	var ime = $("#kreirajIme").val();
+	var priimek = $("#kreirajPriimek").val();
+	var datumRojstva = $("#kreirajDatumRojstva").val();
+	var krajBivanja = $("#kreirajRegijo").val();
+
+	if (!ime || !priimek || !datumRojstva || ime.trim().length == 0 ||
+      priimek.trim().length == 0 || datumRojstva.trim().length == 0 || krajBivanja.trim().length == 0) {
+		$("#kreirajSporocilo").html("<span class='obvestilo label " +
+      "label-warning fade-in'>Prosim vnesite zahtevane podatke!</span>");
+	} else {
+		$.ajaxSetup({
+		    headers: {"Ehr-Session": sessionId}
+		});
+		$.ajax({
+		    url: baseUrl + "/ehr",
+		    type: 'POST',
+		    success: function (data) {
+		        var ehrId = data.ehrId;
+		        var partyData = {
+		            firstNames: ime,
+		            lastNames: priimek,
+		            dateOfBirth: datumRojstva,
+		            address: krajBivanja;
+		            partyAdditionalInfo: [{key: "ehrId", value: ehrId}]
+		        };
+		        $.ajax({
+		            url: baseUrl + "/demographics/party",
+		            type: 'POST',
+		            contentType: 'application/json',
+		            data: JSON.stringify(partyData),
+		            success: function (party) {
+		                if (party.action == 'CREATE') {
+		                    $("#kreirajSporocilo").html("<span class='obvestilo " +
+                          "label label-success fade-in'>Uspešno kreiran EHR '" +
+                          ehrId + "'.</span>");
+		                    $("#preberiEHRid").val(ehrId);
+		                }
+		            },
+		            error: function(err) {
+		            	$("#kreirajSporocilo").html("<span class='obvestilo label " +
+                    "label-danger fade-in'>Napaka '" +
+                    JSON.parse(err.responseText).userMessage + "'!");
+		            }
+		        });
+		    }
+		});
+	}
+}
+
+
+/**
+ * Za podan EHR ID preberi demografske podrobnosti pacienta in izpiši sporočilo
+ * s pridobljenimi podatki (ime, priimek in datum rojstva).
+ */
+function preberiEHRodBolnika() {
+	sessionId = getSessionId();
+
+	var ehrId = $("#preberiEHRid").val();
+
+	if (!ehrId || ehrId.trim().length == 0) {
+		$("#preberiSporocilo").html("<span class='obvestilo label label-warning " +
+      "fade-in'>Prosim vnesite zahtevan podatek!");
+	} else {
+		$.ajax({
+			url: baseUrl + "/demographics/ehr/" + ehrId + "/party",
+			type: 'GET',
+			headers: {"Ehr-Session": sessionId},
+	    	success: function (data) {
+				var party = data.party;
+				$("#preberiSporocilo").html("<span class='obvestilo label " +
+          "label-success fade-in'>Bolnik '" + party.firstNames + " " +
+          party.lastNames + "', ki se je rodil '" + party.dateOfBirth + party.address +
+          "'.</span>");
+			},
+			error: function(err) {
+				$("#preberiSporocilo").html("<span class='obvestilo label " +
+          "label-danger fade-in'>Napaka '" +
+          JSON.parse(err.responseText).userMessage + "'!");
+			}
+		});
+	}
+}
+
+//uporabi drugi vir
+function preberiDejavnikeTveganja() {
+	sessionId = getSessionId();
+
+	var ehrId = $("#preberiEHRid").val();
+
+	}
+}
+
+
+
+
+$(document).ready(function() {
+
+  /**
+   * Napolni testne vrednosti (ime, priimek in datum rojstva) pri kreiranju
+   * EHR zapisa za novega bolnika, ko uporabnik izbere vrednost iz
+   * padajočega menuja 
+   */
+  $('#preberiPredlogoBolnika').change(function() {
+    $("#kreirajSporocilo").html("");
+    var podatki = $(this).val().split(",");
+    $("#kreirajIme").val(podatki[0]);
+    $("#kreirajPriimek").val(podatki[1]);
+    $("#kreirajDatumRojstva").val(podatki[2]);
+    $("#kreirajKraj").val(podatki[3]);
+  });
+
+  /**
+   * Napolni testni EHR ID pri prebiranju EHR zapisa obstoječega bolnika,
+   * ko uporabnik izbere vrednost iz padajočega menuja
+   */
+	$('#preberiObstojeciEHR').change(function() {
+		$("#preberiSporocilo").html("");
+		$("#preberiEHRid").val($(this).val());
+	});
+
+
+
+    $('#preberiDejavnikeTveganja').change(function() {
+		$("#preberiSporocilo").html("");
+		$("#preberiEHRid").val($(this).val());
+	});
+
+
+});
